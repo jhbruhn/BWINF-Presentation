65cd11456e337f0c4c9fbf2dc9cd9b68785ed946
Jan-Henrik Bruhn <jhbruhn@jan-henriks-mbp.multi.box>
2014-03-16 08:11:15 +0100

    initial

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node plugins/multiplexer
---
+++ plugin/multiplex/client.js
@@ -0,0 +1,14 @@
+(function() {
+	var multiplex = Reveal.getConfig().multiplex;
+	var socketId = multiplex.id;
+	var socket = io.connect(multiplex.url);
+	console.log(socketId);
+	socket.on(multiplex.id, function(data) {
+		// ignore data from sockets that aren't ours
+		console.log(data);
+		if (data.socketId !== socketId) { return; }
+		if( window.location.host === 'localhost:1947' ) return;
+
+		Reveal.slide(data.indexh, data.indexv, data.indexf, 'remote');
+	});
+}());
