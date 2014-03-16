9f93a31935770a6d098bcaa692be7b0c08f254b2
Jan-Henrik Bruhn <jh.bruhn@me.com>
2014-03-16 09:57:08 +0100

    changed theme and added testing equations of doom

--- plugin/multiplex/client.js
+++ plugin/multiplex/client.js
@@ -2,7 +2,7 @@
 	var multiplex = Reveal.getConfig().multiplex;
 	var socketId = multiplex.id;
 	var socket = io.connect(multiplex.url);
-	console.log(socketId);
+
 	socket.on(multiplex.id, function(data) {
 		// ignore data from sockets that aren't ours
 		console.log(data);
